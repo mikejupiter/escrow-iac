2a6898a7d4ba545963a6ff01298574b6666de288
Mike G <Mikeg@theserengeti.io>
2023-11-10 10:02:56 -0800

    Add ssh security group to sample VM

--- test-vm/main.tf
+++ test-vm/main.tf
@@ -7,6 +7,19 @@ resource "aws_key_pair" "example_key" {
   public_key = file("../secrets/example_id_rsa.pub")  
 }
 
+
+resource "aws_security_group" "example_ssh_sg" {
+  name        = "example-ssh-sg"
+  description = "Allow inbound SSH traffic"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from any IP address
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
@@ -16,6 +29,8 @@ resource "aws_instance" "example" {
   tags = {
     Name        = "example"  
   }
+
+  vpc_security_group_ids = [aws_security_group.example_ssh_sg.id]
 }
 
 output "instance_id" {
