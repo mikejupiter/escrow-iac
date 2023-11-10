64bfc54c25c8c5df7241e5338b6fe7359168ca0c
Mike G <Mikeg@theserengeti.io>
2023-11-10 09:42:19 -0800

    Add ID and ssh to sample VM

--- test-vm/main.tf
+++ test-vm/main.tf
@@ -2,7 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_key_pair" "example_key" {
+  key_name   = "example-key"
+  public_key = file("../secrets/example_id_rsa.pub")  
+
 resource "aws_instance" "example" {
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
+
+  key_name      = aws_key_pair.example_key.key_name  
+
+  tags = {
+    Name        = "example"  
+    InstanceID  = aws_instance.example.id  
+  }
 }
