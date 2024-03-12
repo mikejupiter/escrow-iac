a1343d4e94f987fef172c0d90712614cc75bbec0
Mike G <Mikeg@theserengeti.io>
2024-03-11 22:15:23 -0700

    SMTP and ECS service v2

--- smtp/main.tf
+++ smtp/main.tf
@@ -2,42 +2,38 @@ provider "aws" {
   region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_iam_user" "ses_smtp_user" {
-  name = "ses-smtp-user"
+
+resource "aws_iam_user" "smtp_user" {
+  name = "smtp_user"
 }
 
-resource "aws_iam_access_key" "ses_smtp_access_key" {
-  user = aws_iam_user.ses_smtp_user.name
+resource "aws_iam_access_key" "smtp_user" {
+  user = aws_iam_user.smtp_user.name
 }
 
-resource "aws_iam_user_policy" "ses_smtp_policy" {
-  name   = "ses-smtp-policy"
-  user   = aws_iam_user.ses_smtp_user.name
-
-  policy = <<EOF
-{
-  "Version": "2024-03-11",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ses:SendRawEmail",
-        "ses:SendEmail"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_ses_smtp_credential" "ses_smtp_credentials" {
-  user = aws_iam_user.ses_smtp_user.name
-}
-
-output "ses_smtp_user_credentials" {
-  value = {
-    username = aws_iam_user.ses_smtp_user.name
-    password = aws_ses_smtp_credential.ses_smtp_credentials.smtp_password
+data "aws_iam_policy_document" "ses_sender" {
+  statement {
+    actions   = ["ses:SendRawEmail"]
+    resources = ["*"]
   }
 }
+
+resource "aws_iam_policy" "ses_sender" {
+  name        = "ses_sender"
+  description = "Allows sending of e-mails via Simple Email Service"
+  policy      = data.aws_iam_policy_document.ses_sender.json
+}
+
+resource "aws_iam_user_policy_attachment" "test-attach" {
+  user       = aws_iam_user.smtp_user.name
+  policy_arn = aws_iam_policy.ses_sender.arn
+}
+
+output "smtp_username" {
+  value = aws_iam_access_key.smtp_user.id
+}
+
+output "smtp_password" {
+  value = aws_iam_access_key.smtp_user.ses_smtp_password_v4
+}
+
