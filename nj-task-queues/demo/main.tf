0fc5ed10f1a7c91a544a731c90e9034914e394a8
Mike G <Mikeg@theserengeti.io>
2024-04-08 09:54:25 -0700

    different envs

--- nj-task-queues/demo/main.tf
+++ nj-task-queues/demo/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 # Define the main SQS queue
-resource "aws_sqs_queue" "etickets_for_upload" {
-  name                      = "etickets_for_upload"
+resource "aws_sqs_queue" "demo_etickets_for_upload" {
+  name                      = "demo_etickets_for_upload"
   delay_seconds             = 0  # 20 minutes delay
   max_message_size          = 262144  # 256 KB message size limit
   message_retention_seconds = 86400  # Messages will be retained for 1 day
@@ -13,19 +13,19 @@ resource "aws_sqs_queue" "etickets_for_upload" {
 
 
 # Define the failure processing queue
-resource "aws_sqs_queue" "etickets_for_upload_failure" {
-  name = "etickets_for_upload_failure"
+resource "aws_sqs_queue" "demo_etickets_for_upload_failure" {
+  name = "demo_etickets_for_upload_failure"
 }
 
 
 # Create IAM user sts_access_dev
-resource "aws_iam_user" "sts_access_dev" {
-  name = "sts_access_dev"
+resource "aws_iam_user" "sts_access_demo" {
+  name = "sts_access_demo"
 }
 
 # Create IAM policy allowing receive and send messages to the main SQS queue
-resource "aws_iam_policy" "sqs_access_policy" {
-  name        = "sqs_access_policy"
+resource "aws_iam_policy" "sqs_access_policy_demo" {
+  name        = "sqs_access_policy_demo"
   description = "Allows receive and send messages to the main SQS queue"
 
   policy = jsonencode({
@@ -39,8 +39,8 @@ resource "aws_iam_policy" "sqs_access_policy" {
           "sqs:SendMessage"
         ],
         Resource  = [
-          aws_sqs_queue.etickets_for_upload.arn,
-          aws_sqs_queue.etickets_for_upload_failure.arn
+          aws_sqs_queue.demo_etickets_for_upload.arn,
+          aws_sqs_queue.demo_etickets_for_upload_failure.arn
         ]
       }
     ]
@@ -48,8 +48,8 @@ resource "aws_iam_policy" "sqs_access_policy" {
 }
 
 # Attach the policy to the IAM user
-resource "aws_iam_user_policy_attachment" "attach_policy_to_user" {
-  user       = aws_iam_user.sts_access_dev.name
-  policy_arn = aws_iam_policy.sqs_access_policy.arn
+resource "aws_iam_user_policy_attachment" "attach_policy_to_user_demo" {
+  user       = aws_iam_user.sts_access_demo.name
+  policy_arn = aws_iam_policy.sqs_access_policy_demo.arn
 }
 
---
+++ nj-task-queues/dev/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1"  # Specify your desired AWS region
+}
+
+# Define the main SQS queue
+resource "aws_sqs_queue" "dev_etickets_for_upload" {
+  name                      = "dev_etickets_for_upload"
+  delay_seconds             = 0  # 20 minutes delay
+  max_message_size          = 262144  # 256 KB message size limit
+  message_retention_seconds = 86400  # Messages will be retained for 1 day
+  visibility_timeout_seconds = 600  # Default visibility timeout
+}
+
+
+# Define the failure processing queue
+resource "aws_sqs_queue" "dev_etickets_for_upload_failure" {
+  name = "dev_etickets_for_upload_failure"
+}
+
+
+# Create IAM user sts_access_dev
+resource "aws_iam_user" "sts_access_dev" {
+  name = "sts_access_dev"
+}
+
+# Create IAM policy allowing receive and send messages to the main SQS queue
+resource "aws_iam_policy" "sqs_access_policy_dev" {
+  name        = "sqs_access_policy_dev"
+  description = "Allows receive and send messages to the main SQS queue"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Action    = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:SendMessage"
+        ],
+        Resource  = [
+          aws_sqs_queue.dev_etickets_for_upload.arn,
+          aws_sqs_queue.dev_etickets_for_upload_failure.arn
+        ]
+      }
+    ]
+  })
+}
+
+# Attach the policy to the IAM user
+resource "aws_iam_user_policy_attachment" "attach_policy_to_user_dev" {
+  user       = aws_iam_user.sts_access_dev.name
+  policy_arn = aws_iam_policy.sqs_access_policy_dev.arn
+}
+
