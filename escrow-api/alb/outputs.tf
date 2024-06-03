8e497d881fb045490f282a12fde6938420d61c79
Mike G <Mikeg@theserengeti.io>
2024-06-03 12:14:39 -0700

    alb remove sg output

--- escrow-api/alb/outputs.tf
+++ escrow-api/alb/outputs.tf
@@ -6,9 +6,6 @@ output "escrow_alb_dns_name" {
   value = aws_lb.escrow_alb.dns_name
 }
 
-output "alb_security_group_id" {
-  value = aws_security_group.alb_sg.id
-}
 
 output "target_group_arn" {
   value = aws_lb_target_group.escrow_api_target_group.arn
